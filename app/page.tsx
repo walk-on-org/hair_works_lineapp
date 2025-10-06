069f47d9210432e4328eb629482d1f4faefe31a4
k.shimamura <34276109+KeisukeShimamura@users.noreply.github.com>
2025-10-06 17:15:33 +0900

    TOP画面にLINEログインおよび、ログイン情報を表示

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,36 @@
-import { Liff } from "@/app/_components/Liff";
+"use client";
+
+import { useEffect, useState } from "react";
+import { useGlobalContext } from "@/hooks/useGlobalContext";
+import { Profile } from "@liff/get-profile";
 
 export default function Home() {
+  const { liff, liffError } = useGlobalContext();
+  const [profile, setProfile] = useState<Profile | null>(null);
+
+  useEffect(() => {
+    if (liff?.isLoggedIn()) {
+      (async () => {
+        const profile = await liff.getProfile();
+        setProfile(profile);
+      })();
+    } else {
+      liff?.login();
+    }
+  }, [liff]);
+
   return (
     <main>
-      <Liff />
+      {liff && <p>LIFF init succeeded.</p>}
+      {liffError && <p>LIFF init failed.</p>}
+      {profile && (
+        <>
+          <p>Display Name: {profile.displayName}</p>
+          <p>User ID: {profile.userId}</p>
+          <p>Picture URL: {profile.pictureUrl}</p>
+          <p>Status Message: {profile.statusMessage}</p>
+        </>
+      )}
     </main>
   );
 }
