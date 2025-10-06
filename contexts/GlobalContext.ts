fb0bceec6d2a516d37885a182b00d55d58112db0
k.shimamura <34276109+KeisukeShimamura@users.noreply.github.com>
2025-10-06 15:43:46 +0900

    App Router用にソース修正

---
+++ contexts/GlobalContext.ts
@@ -0,0 +1,13 @@
+"use client";
+
+import { Liff } from "@line/liff";
+import { createContext } from "react";
+
+type GlobalContextType = {
+  liff: Liff | null;
+  liffError: string | null;
+};
+
+export const GlobalContext = createContext<GlobalContextType | undefined>(
+  undefined
+);
---
+++ hooks/useGlobalContext.ts
@@ -0,0 +1,14 @@
+import { GlobalContext } from "@/contexts/GlobalContext";
+import { useContext } from "react";
+
+export const useGlobalContext = () => {
+  const value = useContext(GlobalContext);
+
+  if (!value) {
+    throw new Error(
+      "useGlobalContext must be used within a GlobalContextProvider"
+    );
+  }
+
+  return value;
+};
